104ff84bc3c73c402e3404e71274199acb409317
sai-sai24 <cyleenc24@gmail.com>
2023-11-07 15:24:46 +0800

    changes - sai

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json({limit:'50mb'}));
+
+const products = require('./routes/product');
+
+app.use('/api/v1', products);
 
 module.exports = app
