ee5ccce6d0d1fb6e24194891d0ca4c6f02085a69
sai-sai24 <cyleenc24@gmail.com>
2023-11-07 15:40:17 +0800

    changes - sai

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { getProducts } = require('../controllers/productController');
+
 router.get('/products', getProducts)
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+const connectDatabase = require('./config/database')
 
 const dotenv = require('dotenv');
 dotenv.config({path: 'backend/config/config.env'})
+
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
 	console.log(`server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
