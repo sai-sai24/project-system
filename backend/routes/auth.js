dd173f3233944394aa0a0720e1247edd3fee4c49
sai-sai24 <cyleenc24@gmail.com>
2023-11-16 15:21:43 +0800

    update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,34 +1,21 @@
 const express = require("express");
 
 const router = express.Router();
-
 const getProducts = require("../models/product");
-
 const upload = require("../utils/multer");
 
 const {
   registerUser,
-
   loginUser,
-
   logout,
-
   forgotPassword,
-
   resetPassword,
-
   getUserProfile,
-
   allUsers,
-
   updateProfile,
-
   updatePassword,
-
   getUserDetails,
-
   updateUser,
-
   deleteUser,
 } = require("../controllers/authController");
 
@@ -40,11 +27,8 @@ const {
 
 router
   .route("/admin/user/:id")
-
   .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
-
   .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
-
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 router
@@ -60,25 +44,16 @@ router.put(
 
 router
   .route("/admin/user/:id")
-
   .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
-
   .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
-
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 router.put("/password/update", isAuthenticatedUser, updatePassword);
-
 router.get("/me", isAuthenticatedUser, getUserProfile);
-
 router.post("/register", upload.single("avatar"), registerUser);
-
 router.post("/login", loginUser);
-
 router.route("/password/forgot").post(forgotPassword);
-
 router.route("/password/reset/:token").put(resetPassword);
-
 router.route("/logout").get(logout);
 
 module.exports = router;
